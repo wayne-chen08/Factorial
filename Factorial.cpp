83a9fbc2718e04c5d945a7d0a67462eceb2c59f4
wayne chen <133223266+wayne-chen08@users.noreply.github.com>
2023-06-21 23:47:27 +0800

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -7,9 +7,9 @@ int main()
     for(int i = 0; i < 500; i++){
     	a[i] = 0;
     	b[i] = 0;
-	}
-    int input=0;
-    cout << "please enter number" << endl;
+    }
+    int input = 0;
+    cout << "please enter a number" << endl;
     cin >> input;
     int n = 1;
     
@@ -17,33 +17,34 @@ int main()
     
     while(n <= input){
         for(int i = 0; i < 500; i++){
-                if(a[i]==0) 
-					continue;
-                b[i] = a[i] * n;
+            if(a[i]==0) 
+	       continue;
+            b[i] = a[i] * n;
         }
 
         for(int i = 499; i >= 0; i--){
-				if(b[i] >= 10){
-					b[i - 1] += b[i] / 10;
-					b[i] %= 10;
-				}
-            }
+	    if(b[i] >= 10){
+	    	b[i - 1] += b[i] / 10;
+	    	b[i] %= 10;
+	    }
+        }
             
         for(int i = 0; i < 500; i++){
             a[i] = b[i];
         }
         n++;
     }
-    int non_zero = 0;
+    int non_zero = 0; //找出從第幾位開始不是0
+	
     for(int i = 0; i < sizeof(a) / sizeof(a[0]); i++){
     	if(a[i] != 0){
-    		non_zero = i;
-    		break;
-		}
-	}
-	for(int i = non_zero; i < sizeof(a) / sizeof(a[0]); i++){
-		cout << a[i];
-	}
+    	   non_zero = i;
+    	   break;
+	   }
+    }
+    for(int i = non_zero; i < sizeof(a) / sizeof(a[0]); i++){
+	cout << a[i];
+    }
     return 0;
 
 }
